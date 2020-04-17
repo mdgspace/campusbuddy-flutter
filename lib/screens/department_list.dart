56170698f6d14c4a49ce1798d082e5ebf280b5f9
Prateek Sachan <psachan@cs.iitr.ac.in>
2020-04-17 18:45:27 +0530

    Firebase Integration

--- lib/screens/department_list.dart
+++ lib/screens/department_list.dart
@@ -2,15 +2,61 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DepartmentListPage extends StatelessWidget {
+  // Title of the screen
+  final title;
+  // Doc ID of current area
+  final areaDocID;
+
+  DepartmentListPage({Key key, this.title, this.areaDocID}) : super(key: key);
+
+  Widget _buildListItem(BuildContext context, DocumentSnapshot document) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        side: BorderSide(color: Colors.white70, width: 1),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      margin: EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+      elevation: 5,
+      child: ListTile(
+        contentPadding: EdgeInsets.all(10),
+        leading: SvgPicture.asset(
+          'assets/department_icon.svg',
+          color: Theme.of(context).primaryColor,
+        ),
+        trailing: Icon(
+          Icons.arrow_forward_ios,
+          color: Color(0XFF3B3B3B),
+        ),
+        title: Text(
+          document['dept_name'],
+          style: TextStyle(
+            fontSize: 17,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildList(context, snapshot) {
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        (context, index) {
+          return _buildListItem(context, snapshot.data.documents[index]);
+        },
+        childCount: snapshot.data.documents.length,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: CustomScrollView(
         slivers: <Widget>[
           SliverAppBar(
-            title: Container(),
             leading: Icon(
               Icons.arrow_back,
               color: Colors.white,
@@ -18,60 +64,41 @@ class DepartmentListPage extends StatelessWidget {
             pinned: true,
             flexibleSpace: FlexibleSpaceBar(
               centerTitle: true,
-              title: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: <Widget>[
-                  const Text(
-                    "Academic Departments & Centres",
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                      fontSize: 15,
-                    ),
-                  ),
-                ],
+              // TODO: Add proper padding to prevent overflow
+              title: Text(
+                title,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(
+                  fontSize: 14,
+                ),
               ),
               background: Padding(
                 padding: EdgeInsets.all(70),
                 child: SvgPicture.asset(
-                  'assets/icon.svg',
+                  'assets/department_icon.svg',
                   color: Colors.white,
                 ),
               ),
             ),
             expandedHeight: 250,
           ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (context, index) {
-                return Card(
-                  shape: RoundedRectangleBorder(
-                    side: BorderSide(color: Colors.white70, width: 1),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  margin: EdgeInsets.symmetric(vertical: 5, horizontal: 15),
-                  // elevation: ,
-                  child: ListTile(
-                    contentPadding: EdgeInsets.all(10),
-                    leading: SvgPicture.asset(
-                      'assets/icon.svg',
-                      color: Theme.of(context).primaryColor,
-                    ),
-                    trailing: Icon(
-                      Icons.arrow_forward_ios,
-                      color: Color(0XFF3B3B3B),
-                    ),
-                    title: Text(
-                      'Item #$index',
-                      style: TextStyle(
-                        fontSize: 17,
-                      ),
-                    ),
-                  ),
-                );
-              },
-              childCount: 1000,
-            ),
+          StreamBuilder<QuerySnapshot>(
+            stream: Firestore.instance
+                .collection('directory/$areaDocID/department_list')
+                .snapshots(),
+            builder:
+                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              // TODO: Better way to represent errors
+              if (snapshot.hasError) return SliverFillRemaining();
+              switch (snapshot.connectionState) {
+                case ConnectionState.waiting:
+                  // TODO: Add loading bar
+                  return SliverFillRemaining();
+                default:
+                  return _buildList(context, snapshot);
+              }
+            },
           )
         ],
       ),
