e27c940e582284bc93b997fcdd9c7b8e7976aa0a
guptaprakhariitr <prakharpguptag@gmail.com>
2020-04-19 16:31:36 +0530

    backColor to indigo[900],fontSizefixed,ScreenUtil used,+Design fixes

--- lib/ContactScreens/ContactList.dart
+++ lib/ContactScreens/ContactList.dart
@@ -4,32 +4,37 @@ import 'package:loading_animations/loading_animations.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:campusbuddy/ContactScreens/ContactListElement.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 class ContactList extends StatefulWidget {
-  static Color color = const Color(0xFF303E84);
+  static Color color = const Color(0xff303e84);
   static String assetName = 'assets/contactPerson.svg';
   static String assetNameAcad = 'assets/acad.svg';
+  static String assetNameArrow = 'assets/arrow.svg';
   @override
   _ContactListState createState() => _ContactListState();
 }
 class _ContactListState extends State<ContactList> {
   final databaseReference = FirebaseDatabase.instance.reference();
-  static String sub="sub";
-  static int length=3;
   static String title="BioTechnology";
   static Widget svgIcon = SvgPicture.asset(
       ContactList.assetName,
       color: ContactList.color,
-      width: 42,
-      height: 42,
+      width: 42.w,
+      height: 42.h,
   );
-
+  static Widget svgArrowIcon = SvgPicture.asset(
+    ContactList.assetNameArrow,
+    color: Color(0xff3B3B3B),
+    width: 5.5.w,
+    height: 11.3.h,
+  );
   static Widget svgIconAcad = Padding(
       padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
       child:SvgPicture.asset(
       ContactList.assetNameAcad,
       color: Colors.white,
-      width: 42,
-      height: 42,
+      width: 42.w,
+      height: 42.h,
   ));
   static final dbRef =  FirebaseDatabase.instance.reference();
   static final makecall= new MakeCall();
@@ -55,14 +60,15 @@ class _ContactListState extends State<ContactList> {
                   floating: true,
                   pinned: true,
                   snap: true,
-                  expandedHeight: 215.3,
+                  expandedHeight: 215.3.h,
                   backgroundColor: ContactList.color,
                   flexibleSpace:FlexibleSpaceBar(
                     centerTitle: true,
                     title: Text(
-                      '${title}',
+                      title,
                       style: TextStyle(
-                        fontSize: 17,
+                        fontSize: ScreenUtil().setSp(20),
+                        fontWeight: FontWeight.w500,
                       ),
                     ),
                     background:svgIconAcad,
@@ -74,45 +80,52 @@ class _ContactListState extends State<ContactList> {
                       // The builder function returns a ListTile with a title that
                       // displays the index of the current item.
                           (context, index)  {return Padding(
-                          padding: EdgeInsets.all(8),
-                          child: Card(
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.start
-                                  ,
-                                  children: [
-                                    Padding(
-                                        padding:EdgeInsets.fromLTRB(12, 17, 12, 17),
-                                        child: svgIcon),
-                                    Column(
-                                      mainAxisAlignment: MainAxisAlignment.center,
-                                      children: [
-                                        Text('${snapshot.data[index].name}',
-                                          style: TextStyle(
-                                            fontSize: 17,
-                                            fontWeight: FontWeight.bold,
-                                          ),),
-                                        Text('${snapshot.data[index].sub}',
-                                          style: TextStyle(
-                                            fontSize: 17,
-                                            color: Colors.black38,
-                                          ),)
-                                      ],
-                                    )
-                                  ],
-                                ),
-                                IconButton(
-                                  icon: Icon(Icons.arrow_right),
-                                  onPressed: () {
-                                    print(snapshot.data[index].name);
-                                    print("hello");
-                                    PassToContact pass=new PassToContact(snapshot.data[index].office.toString(),snapshot.data[index].email.toString(),snapshot.data[index].residence.toString(),snapshot.data[index].name.toString(),snapshot.data[index].sub.toString(),title);
-                                Navigator.pushNamed(context, '/Contact',arguments: pass);
-                                  },
-                                )
-                              ],
+                          padding: EdgeInsets.fromLTRB(13.5, 8, 13.5, 0),
+                          child: GestureDetector(
+                            onTap: (){
+                              PassToContact pass=new PassToContact(snapshot.data[index].office.toString(),snapshot.data[index].email.toString(),snapshot.data[index].residence.toString(),snapshot.data[index].name.toString(),snapshot.data[index].sub.toString(),title);
+                              Navigator.pushNamed(context, '/Contact',arguments: pass);
+                            },
+                            child: Card(
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start
+                                    ,
+                                    children: [
+                                      Padding(
+                                          padding:EdgeInsets.fromLTRB(12, 17, 12, 17),
+                                          child: svgIcon),
+                                      Column(
+                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        children: [
+                                          Text('${snapshot.data[index].name}',
+                                            style: TextStyle(
+                                              fontSize: ScreenUtil().setSp(17),
+
+                                              fontWeight: FontWeight.normal,
+                                            ),),
+                                          Text('${snapshot.data[index].sub}',
+                                            style: TextStyle(
+                                              fontSize: ScreenUtil().setSp(17),
+                                              color: Colors.black38,
+                                            ),)
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                  IconButton(
+                                    icon: svgArrowIcon,
+                                    onPressed: () {
+                                      print(snapshot.data[index].name);
+                                      print("hello");
+                                      PassToContact pass=new PassToContact(snapshot.data[index].office.toString(),snapshot.data[index].email.toString(),snapshot.data[index].residence.toString(),snapshot.data[index].name.toString(),snapshot.data[index].sub.toString(),title);
+                                  Navigator.pushNamed(context, '/Contact',arguments: pass);
+                                    },
+                                  )
+                                ],
+                              ),
                             ),
                           )
                       );},
@@ -129,7 +142,7 @@ class _ContactListState extends State<ContactList> {
       });
   @override
   Widget build(BuildContext context) {
-    print("heelo");
+    ScreenUtil.init(context,allowFontScaling: true, width: 410, height: 703);
     return Scaffold(
       body:futureBuilder,
     );
